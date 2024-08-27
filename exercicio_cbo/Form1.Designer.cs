5268a99098025d3da636c07b404af42d2c4e8e1f
Usuario <Usuario@CPD02-M14>
2024-08-27 12:22:40 -0300

    alterações no projeto.

--- exercicio_cbo/Form1.Designer.cs
+++ exercicio_cbo/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblTodos = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -47,33 +47,36 @@
             this.btnSudeste = new System.Windows.Forms.Button();
             this.btnSul = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
+            this.lblNorte = new System.Windows.Forms.Label();
+            this.lblNordeste = new System.Windows.Forms.Label();
+            this.lblCentro = new System.Windows.Forms.Label();
+            this.lblSudeste = new System.Windows.Forms.Label();
+            this.lblSul = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label1.Location = new System.Drawing.Point(126, 38);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(113, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "Todos os estados";
             // 
-            // label2
+            // lblTodos
             // 
-            this.label2.Location = new System.Drawing.Point(544, 43);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 23);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.lblTodos.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTodos.Location = new System.Drawing.Point(514, 43);
+            this.lblTodos.Name = "lblTodos";
+            this.lblTodos.Size = new System.Drawing.Size(100, 23);
+            this.lblTodos.TabIndex = 1;
+            this.lblTodos.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(126, 85);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(126, 90);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(70, 13);
             this.label3.TabIndex = 2;
@@ -82,7 +85,8 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(126, 130);
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(126, 142);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(87, 13);
             this.label4.TabIndex = 3;
@@ -91,6 +95,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label5.Location = new System.Drawing.Point(126, 188);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(106, 13);
@@ -100,6 +105,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.label6.Location = new System.Drawing.Point(126, 239);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(83, 13);
@@ -159,11 +165,12 @@
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 21);
             this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // comboBox3
             // 
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(259, 127);
+            this.comboBox3.Location = new System.Drawing.Point(259, 134);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(121, 21);
             this.comboBox3.TabIndex = 9;
@@ -200,24 +207,27 @@
             this.btnNorte.TabIndex = 13;
             this.btnNorte.Text = "—";
             this.btnNorte.UseVisualStyleBackColor = true;
+            this.btnNorte.Click += new System.EventHandler(this.btnNorte_Click);
             // 
             // btnNordeste
             // 
-            this.btnNordeste.Location = new System.Drawing.Point(426, 127);
+            this.btnNordeste.Location = new System.Drawing.Point(426, 132);
             this.btnNordeste.Name = "btnNordeste";
             this.btnNordeste.Size = new System.Drawing.Size(54, 23);
             this.btnNordeste.TabIndex = 14;
             this.btnNordeste.Text = "—";
             this.btnNordeste.UseVisualStyleBackColor = true;
+            this.btnNordeste.Click += new System.EventHandler(this.btnNordeste_Click);
             // 
             // btnCentro
             // 
-            this.btnCentro.Location = new System.Drawing.Point(426, 178);
+            this.btnCentro.Location = new System.Drawing.Point(426, 183);
             this.btnCentro.Name = "btnCentro";
             this.btnCentro.Size = new System.Drawing.Size(54, 23);
             this.btnCentro.TabIndex = 15;
             this.btnCentro.Text = "—";
             this.btnCentro.UseVisualStyleBackColor = true;
+            this.btnCentro.Click += new System.EventHandler(this.btnCentro_Click);
             // 
             // btnSudeste
             // 
@@ -227,6 +237,7 @@
             this.btnSudeste.TabIndex = 16;
             this.btnSudeste.Text = "—";
             this.btnSudeste.UseVisualStyleBackColor = true;
+            this.btnSudeste.Click += new System.EventHandler(this.btnSudeste_Click);
             // 
             // btnSul
             // 
@@ -236,71 +247,79 @@
             this.btnSul.TabIndex = 17;
             this.btnSul.Text = "—";
             this.btnSul.UseVisualStyleBackColor = true;
+            this.btnSul.Click += new System.EventHandler(this.btnSul_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(126, 282);
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(126, 285);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(59, 13);
             this.label7.TabIndex = 18;
             this.label7.Text = "Região Sul";
             // 
-            // label8
+            // lblNorte
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(544, 93);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(35, 13);
-            this.label8.TabIndex = 19;
-            this.label8.Text = "label8";
+            this.lblNorte.AutoSize = true;
+            this.lblNorte.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblNorte.Location = new System.Drawing.Point(514, 93);
+            this.lblNorte.Name = "lblNorte";
+            this.lblNorte.Size = new System.Drawing.Size(0, 13);
+            this.lblNorte.TabIndex = 19;
+            this.lblNorte.Click += new System.EventHandler(this.lblNorte_Click);
             // 
-            // label9
+            // lblNordeste
             // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(544, 135);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(35, 13);
-            this.label9.TabIndex = 20;
-            this.label9.Text = "label9";
+            this.lblNordeste.AutoSize = true;
+            this.lblNordeste.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblNordeste.Location = new System.Drawing.Point(514, 137);
+            this.lblNordeste.Name = "lblNordeste";
+            this.lblNordeste.Size = new System.Drawing.Size(0, 13);
+            this.lblNordeste.TabIndex = 20;
+            this.lblNordeste.Click += new System.EventHandler(this.lblNordeste_Click);
             // 
-            // label10
+            // lblCentro
             // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(538, 180);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(41, 13);
-            this.label10.TabIndex = 21;
-            this.label10.Text = "label10";
+            this.lblCentro.AutoSize = true;
+            this.lblCentro.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblCentro.Location = new System.Drawing.Point(514, 188);
+            this.lblCentro.Name = "lblCentro";
+            this.lblCentro.Size = new System.Drawing.Size(0, 13);
+            this.lblCentro.TabIndex = 21;
+            this.lblCentro.Click += new System.EventHandler(this.lblCentro_Click);
             // 
-            // label11
+            // lblSudeste
             // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(538, 234);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(41, 13);
-            this.label11.TabIndex = 22;
-            this.label11.Text = "label11";
+            this.lblSudeste.AutoSize = true;
+            this.lblSudeste.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblSudeste.Location = new System.Drawing.Point(514, 239);
+            this.lblSudeste.Name = "lblSudeste";
+            this.lblSudeste.Size = new System.Drawing.Size(0, 13);
+            this.lblSudeste.TabIndex = 22;
+            this.lblSudeste.Click += new System.EventHandler(this.lblSudeste_Click);
             // 
-            // label12
+            // lblSul
             // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(538, 287);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(41, 13);
-            this.label12.TabIndex = 23;
-            this.label12.Text = "label12";
+            this.lblSul.AutoSize = true;
+            this.lblSul.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblSul.Location = new System.Drawing.Point(514, 287);
+            this.lblSul.Name = "lblSul";
+            this.lblSul.Size = new System.Drawing.Size(0, 13);
+            this.lblSul.TabIndex = 23;
+            this.lblSul.Click += new System.EventHandler(this.lblSul_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label12);
-            this.Controls.Add(this.label11);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblSul);
+            this.Controls.Add(this.lblSudeste);
+            this.Controls.Add(this.lblCentro);
+            this.Controls.Add(this.lblNordeste);
+            this.Controls.Add(this.lblNorte);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.btnSul);
             this.Controls.Add(this.btnSudeste);
@@ -318,10 +337,11 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTodos);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -330,7 +350,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTodos;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
@@ -348,11 +368,11 @@
         private System.Windows.Forms.Button btnSudeste;
         private System.Windows.Forms.Button btnSul;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label lblNorte;
+        private System.Windows.Forms.Label lblNordeste;
+        private System.Windows.Forms.Label lblCentro;
+        private System.Windows.Forms.Label lblSudeste;
+        private System.Windows.Forms.Label lblSul;
     }
 }
 
--- exercicio_cbo/Form1.cs
+++ exercicio_cbo/Form1.cs
@@ -19,7 +19,123 @@ namespace exercicio_cbo
 
         private void btnTodos_Click(object sender, EventArgs e)
         {
+            string est = comboBox1.Text.ToString();
+            if (est == " São Paulo - SP" | est == " Espírito Santo - ES" | est == " Rio de Janeiro - RJ" | est == " Minas Gerais - MG")
+            {
+                comboBox5.Items.Add(comboBox5.SelectedItem);
+                comboBox1.Items.Remove(comboBox1.SelectedItem);
+                lblTodos.Text = comboBox1.SelectedItem.ToString();
+                lblSudeste.Text = comboBox5.SelectedItem.ToString();
+            }
+            else if (est== " Alagoas - AL" | est== " Bahia - BA" | est == " Ceará - CE"| est== " Maranhão - MA" | est== " Paraíba - PB" | est== " Pernambuco - PE" | est== " Piauí - PI" | est== " Rio Grande do Norte - RN" |est== " Sergipe - SE")
+            {
+                comboBox3.Items.Add(comboBox3.SelectedItem);
+                comboBox1.Items.Remove(comboBox1.SelectedItem);
+                lblTodos.Text = comboBox1.SelectedItem.ToString();
+                lblNordeste.Text = comboBox3.SelectedItem.ToString();
+            }
+            else if (est== " Amazonas -  AM" | est== " Pará - PA" | est== " Acre - AC" | est== "Roraima - RR" | est== " Rondônia - RO" | est== " Amapá - AP" | est== " Tocantins - TO")
+            {
+                comboBox2.Items.Add(comboBox2.SelectedItem);
+                comboBox1.Items.Remove(comboBox1.SelectedItem);
+                lblTodos.Text = comboBox1.SelectedItem.ToString();
+                lblNorte.Text = comboBox2.SelectedItem.ToString();
+            }
+            else if (est== " Goiás - GO" | est== " Mato Grosso - MT"| est== " Mato Grosso do Sul - MS" | est== " Distrito Federal - DF")
+            {
+                comboBox4.Items.Add(comboBox4.SelectedItem);
+                comboBox1.Items.Remove(comboBox1.SelectedItem);
+                lblTodos.Text = comboBox1.SelectedItem.ToString();
+                lblCentro.Text = comboBox4.SelectedItem.ToString();
+            }
+            else if (est== " Paraná - PR" | est== " Santa Catarina - SC" | est== " Rio Grande do Sul - RS")
+            {
+                comboBox6.Items.Add(comboBox6.SelectedItem);
+                comboBox1.Items.Remove(comboBox1.SelectedItem);
+                lblTodos.Text = comboBox1.SelectedItem.ToString();
+                lblSul.Text = comboBox6.SelectedItem.ToString();
+            }
+                    
+        }
 
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            lblTodos.Text = comboBox1.Items.Count.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void lblNorte_Click(object sender, EventArgs e)
+        {
+            lblNorte.Text = comboBox2.Items.Count.ToString();
+        }
+
+        private void lblNordeste_Click(object sender, EventArgs e)
+        {
+            lblNordeste.Text = comboBox3.Items.Count.ToString();
+        }
+
+        private void lblCentro_Click(object sender, EventArgs e)
+        {
+            lblCentro.Text = comboBox4.Items.Count.ToString();
+        }
+
+        private void lblSudeste_Click(object sender, EventArgs e)
+        {
+            lblSudeste.Text = comboBox5.Items.Count.ToString();
+        }
+
+        private void lblSul_Click(object sender, EventArgs e)
+        {
+            lblSul.Text = comboBox6.Items.Count.ToString();
+        }
+
+        private void btnNorte_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(comboBox2.SelectedItem);
+            comboBox2.Items.Remove(comboBox2.SelectedItem);
+            lblNorte.Text = comboBox2.Items.Count.ToString();
+            lblTodos.Text = comboBox1.Items.Count.ToString();
+        }
+
+        private void btnNordeste_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(comboBox3.SelectedItem);
+            comboBox3.Items.Remove(comboBox3.SelectedItem);
+            lblNordeste.Text = comboBox3.Items.Count.ToString();
+            lblTodos.Text = comboBox1.Items.Count.ToString();
+        }
+
+        private void btnCentro_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(comboBox4.SelectedItem);
+            comboBox4.Items.Remove(comboBox4.SelectedItem);
+            lblCentro.Text = comboBox4.Items.Count.ToString();
+            lblTodos.Text = comboBox1.Items.Count.ToString();
+        }
+
+        private void btnSudeste_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(comboBox5.SelectedItem);
+            comboBox5.Items.Remove(comboBox5.SelectedItem);
+            lblSudeste.Text = comboBox5.Items.Count.ToString();
+            lblTodos.Text = comboBox1.Items.Count.ToString();
+        }
+
+        private void btnSul_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(comboBox6.SelectedItem);
+            comboBox6.Items.Remove(comboBox6.SelectedItem);
+            lblSul.Text = comboBox6.Items.Count.ToString();
+            lblTodos.Text = comboBox1.Items.Count.ToString();
         }
     }
 }
